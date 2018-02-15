8179105b019809903934bd7fb3493fd47e90dd7f
325C/PE <36476870+Public-Enemy@users.noreply.github.com>
2018-02-15 18:40:22 +0700

    Update Mahasiswa.java

--- Latihan Class Mahasiswa Dosen/Mahasiswa.java
+++ Latihan Class Mahasiswa Dosen/Mahasiswa.java
@@ -1,39 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package latihanprequiz;
 
-/**
- *
- * @author secondary
- */
-public class Mahasiswa extends Orang{
+public class Mahasiswa extends Orang{ // Extends menandakan Turunan dari class Orang
     
-    private String NIM;
-    private String kelas;
-    private int semester;
-    private MahasiswaReport report;
+    private String NIM; // deklrasi atribute nim bertipe data string
+    private String kelas; // deklrasi atribute kelas bertipe data string
+    private int semester; // deklrasi atribute semester bertipe data integer
+    private MahasiswaReport report; // deklrasi atribute nim bertipe data MahasiswaReport ( Tipe Data Objek )
 
     public Mahasiswa(String nama, String alamat, String kota, int umur, char jenis_kelamin, String NIM, String kelas, int semester, MahasiswaReport Report)
     {
-        super.setnama(nama);
-        super.setalamat(alamat);
-        super.setkota(kota);
-        super.setumur(umur);
-        super.setjenis_kelamin(jenis_kelamin);
-        this.NIM = NIM;
-        this.kelas = kelas;
-        this.semester = semester;
-        this.report = Report;
+        super.setnama(nama); // Menanggil method parent (Orang.java) : setnama()
+        super.setalamat(alamat); // Menanggil method parent (Orang.java) : setalamat()
+        super.setkota(kota); // Menanggil method parent (Orang.java) : setkota()
+        super.setumur(umur); // Menanggil method parent (Orang.java) : setumur()
+        super.setjenis_kelamin(jenis_kelamin); // Menanggil method parent (Orang.java) : setjenis_kelamin()
+        this.NIM = NIM; // Meng-inisialisasi nilai untuk attribute NIM pada kelas ini
+        this.kelas = kelas; // Meng-inisialisasi nilai untuk attribute kelas pada kelas ini
+        this.semester = semester; // Meng-inisialisasi nilai untuk attribute semester pada kelas ini
+        this.report = Report; // Meng-inisialisasi nilai untuk attribute report pada kelas ini
     }
     @Override
     public String toString() {
-        System.out.println("Informasi Mahasiswa\n" +
-"============================");
-        super.toString();
-        System.out.println("NIM\t:" + this.NIM + "\nSemester\t:" + this.kelas + "\n");
+        System.out.println("Informasi Mahasiswa\n" +"============================"); // menapilkan judul laporan
+        super.toString(); // menampilkan isi laporan dari parent class
+        System.out.println("NIM\t:" + this.NIM + "\nSemester\t:" + this.kelas + "\n"); // Menampilkan laporan dari kelas ini
         return "";
     }
 }
