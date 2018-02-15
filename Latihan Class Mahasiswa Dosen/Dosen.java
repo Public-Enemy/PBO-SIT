abbc7ea7ecae460511d10f40ff466190c3456a1f
325C/PE <36476870+Public-Enemy@users.noreply.github.com>
2018-02-15 18:21:02 +0700

    Update Dosen.java

--- Latihan Class Mahasiswa Dosen/Dosen.java
+++ Latihan Class Mahasiswa Dosen/Dosen.java
@@ -1,34 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package latihanprequiz;
 
-/**
- *
- * @author secondary
- */
 public class Dosen extends Orang{
-   private String mk;
-    private String strata;
-    private int salary;
+   private String mk; // Attribute untuk Nama Mata Kuliah
+    private String strata; // Attribute untuk Strata Dosen
+    private int salary; // Attribute untuk gaji dosen
 
     
     public Dosen(String nama , String alamat,String kota,int umur,char jenis_kelamin,String mk, String strata , int salary){
-        super.setnama(nama);
-        super.setalamat(alamat);
-        super.setkota(kota);
-        super.setumur(umur);
-        super.setjenis_kelamin(jenis_kelamin);
-        this.mk = mk;
-        this.strata = strata;
-        this.salary = salary;
+        super.setnama(nama); // Menanggil method parent (Orang.java) : setnama()
+        super.setalamat(alamat); // Menanggil method parent (Orang.java) : setalamat()
+        super.setkota(kota); // Menanggil method parent (Orang.java) : setkota()
+        super.setumur(umur); // Menanggil method parent (Orang.java) : setumur()
+        super.setjenis_kelamin(jenis_kelamin); // Menanggil method parent (Orang.java) : setjenis_kelamin()
+        this.mk = mk; // Meng-inisialisasi nilai untuk attribute mk pada kelas Dosen
+        this.strata = strata; // Meng-inisialisasi nilai untuk attribute strata pada kelas Dosen
+        this.salary = salary; // Meng-inisialisasi nilai untuk attribute salary pada kelas Dosen
     }
-    @Override
+    @Override // untuk meninisialisasi ulang method modifikasi karena method ini tersedia di parent kelas ini (Orang.Java)
     public String toString(){
-        System.out.println("    Informasi Dosen\n" +
-"    ============================\n"+"Nama\t:" + super.getNama() +"\nAlamat\t:"+ super.getAlamat() +"\nKota\t:"+super.getKota() +"\nUmur\t:"+ super.getUmur()+"\nMK\t:"+this.mk +"\nStrata\t:" + this.strata + "\nSalary\t:" + this.salary);
+        System.out.println("Informasi Dosen\n" +"============================\n"+"Nama\t:" + super.getNama() +"\nAlamat\t:"+ super.getAlamat() +"\nKota\t:"+super.getKota() +"\nUmur\t:"+ super.getUmur()+"\nMK\t:"+this.mk +"\nStrata\t:" + this.strata + "\nSalary\t:" + this.salary); // untuk menampilkan laporan dari method ini
         return "";
     }
 }
